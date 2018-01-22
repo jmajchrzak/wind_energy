4f3e2eb90a89fc4e3ac0e965b8ed4fe95af18eb7
jmajchrzak <30370184+jmajchrzak@users.noreply.github.com>
2018-01-22 19:55:37 +0100

    automaticly searching for first date, new data

--- energy.py
+++ energy.py
@@ -1,4 +1,5 @@
 #exec(open("C:\\Users\KUBA\Dysk Google\python\datascience\energy.py").read())
+#pse.pl/getcsv/-/export/csv/GEN_WIATR/data_od/20180101/data_do/20180122
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,28 +8,12 @@ import matplotlib.dates as mdates
 
 columnnames = ['date', 'hour', 'energy']
 
-df1 = pd.read_csv('GEN_WIATR_20180101_20180120_20180120163514.csv', sep = ';', header = None, names = columnnames, decimal = ',', encoding = 'utf_8')
+df1 = pd.read_csv('GEN_WIATR_20180101_20180122_20180122183511.csv', sep = ';', names = columnnames, skiprows = 1, index_col = False, decimal = ',', encoding = 'utf_8')
 
-end = pd.datetime(2018, 1, 20)
+first_day = df1['date'].iloc[0], df1['hour'].iloc[0]
+first_day = str(first_day[0]) + ' ' + str(first_day[1]) + ':00'
 
-l_date = pd.date_range('2018-01-01 01:00:00', periods = len(df1), freq = 'H')
-
-df1['date_hour'] = l_date
-
-#df1['day'] = df1['date'] % 20180100
-
-#proba stworzenia opisu na os x opisu dat, nieudane
-
-#df1.set_index('date',inplace=True)
-'''
-fig, ax = plt.subplots(figsize=(15,7))
-df1.plot(ax=ax)
-
-ax.xaxis.set_major_locator(mdates.WeekdayLocator())
-
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
-
-'''
+df1['date_hour'] = pd.date_range(first_day, periods = len(df1), freq = 'H')
 
 plt.plot( df1['date_hour'], df1['energy'], color = 'black', label = 'Produkcja')
 plt.title('Generacja energii z źródeł wiatrowych')
@@ -37,6 +22,7 @@ plt.ylabel('Production [MW]')
 #plt.xlim(0)
 plt.grid(True)
 
-
 plt.show()
+
+
 #print(df1)
